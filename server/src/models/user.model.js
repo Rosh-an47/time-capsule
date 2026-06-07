77b2404061296b8d5f3644ee13c9ed8e6fdad270
Rosh-an47 <roshan92461@gmail.com>
2026-06-07 12:29:03 +0530

    Add JWT token generation methods to user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 
@@ -51,6 +52,28 @@ userSchema.pre("save",  async function(next){
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    });
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    });
 }
 
 module.exports = mongoose.model('User', userSchema);
